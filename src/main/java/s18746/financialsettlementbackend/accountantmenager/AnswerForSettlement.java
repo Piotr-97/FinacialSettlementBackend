db0860575a59768ed3ab37f8b5e745c21317627e
Piotr Karolak <karolak.piotr.012@gmail.com>
2023-10-16 22:51:17 +0200

    Added AnswerForSettlement

--- src/main/java/s18746/financialsettlementbackend/accountantmenager/AnswerForSettlement.java
+++ src/main/java/s18746/financialsettlementbackend/accountantmenager/AnswerForSettlement.java
@@ -1,4 +1,37 @@
 package s18746.financialsettlementbackend.accountantmenager;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import s18746.financialsettlementbackend.financialsettelmentsmanager.FinancialSettlement;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "answerforsettlement")
 public class AnswerForSettlement {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Enumerated
+    private AnswerStatus status;
+
+
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "financialsettlement_id")
+    private FinancialSettlement financialSettlement;
+
 }
---
+++ src/main/java/s18746/financialsettlementbackend/accountantmenager/AnswerStatus.java
@@ -0,0 +1,4 @@
+package s18746.financialsettlementbackend.accountantmenager;
+
+public enum AnswerStatus {
+}
